e88e1d43e440ae58712cd54863f4ff64eaabf6c7
apple <apple@apples-MacBook-Pro.local>
2022-07-06 18:31:49 +0300

    i did the task

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
+
+
+weather_list = [73.4,75.2,77,86,87.8]
+
+
+
+
 # printer(elements)
 # - Accepts a list
 # - Prints every element of the list
 def printer(elements):
     # Your code here
-    ...
+    for x in range(len(elements)):
+        print (elements[x])
+    
+
+
+#printer(weather_list)
 
 
 # to_celsius(temperatures)
@@ -14,8 +26,16 @@ def printer(elements):
 # The conversion is:
 #   C = (F - 32) * (5/9)
 def to_celsius(temperatures):
-    # Your code here
-    ...
+    ftoc=[]
+    for x in range(len(temperatures)):
+        result = ((temperatures[x]-32)* (5/9)//1)
+        ftoc.append(result)
+    print("The tempreture in celsius are : " )
+    print(ftoc)
+    return ftoc
+
+to_celsius(weather_list)
+    
 
 
 # hottest_days(temperatures, threshold)
@@ -24,8 +44,16 @@ def to_celsius(temperatures):
 # - Returns a list of temperatures
 #   that exceed the threshold
 def hottest_days(temperatures, threshold):
-    # Your code here
-    ...
+    hotpoint=[]
+    for x in range(len(temperatures)):
+        if(temperatures[x] > threshold):
+            hotpoint.append(temperatures[x])
+    print("The lsit of hotest point are: " )
+    print(hotpoint)
+    return hotpoint
+
+
+hottest_days(weather_list,80)  
 
 
 # log_hottest_days(temperatures, threshhold)
@@ -38,5 +66,13 @@ def hottest_days(temperatures, threshold):
 # hint: you can combine
 #       all previous functions
 def print_hottest_days(temperatures, threshhold):
-    # Your code here
-    ...
+    result =[]
+    result = to_celsius(temperatures)
+    hottest_days(result,threshhold)
+
+    
+    
+
+    
+
+print_hottest_days(weather_list,30)
